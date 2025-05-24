22375afc7bb62b73a2e1f4f2b30fc7685ae83b21
lamdz1102 <lamndhhe182252@fpt.edu.vn>
2025-05-24 17:48:26 +0700

    Khởi tạo các biến

--- FishingHub/src/java/model/Users.java
+++ FishingHub/src/java/model/Users.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author pc
- */
 public class Users {
-    //lam
+    private String fullName;
+    private String email;
+    private String password;
+    private String fishingSkillLevel;
+    private boolean termsAccepted;
+
+    public Users() {}
+
+    public Users(String fullName, String email, String password, String fishingSkillLevel, boolean termsAccepted) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.fishingSkillLevel = fishingSkillLevel;
+        this.termsAccepted = termsAccepted;
+    }
+
+    // getter và setter
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getFishingSkillLevel() { return fishingSkillLevel; }
+    public void setFishingSkillLevel(String fishingSkillLevel) { this.fishingSkillLevel = fishingSkillLevel; }
+
+    public boolean isTermsAccepted() { return termsAccepted; }
+    public void setTermsAccepted(boolean termsAccepted) { this.termsAccepted = termsAccepted; }
 }
